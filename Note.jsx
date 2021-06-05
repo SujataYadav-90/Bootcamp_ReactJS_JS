77caf3106a9f15f3f3c45fbd60cca44f4f34739a
SujataYadav-90 <82049562+SujataYadav-90@users.noreply.github.com>
2021-06-05 17:51:51 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note() {
+  return (
+   <div className="note">
+     <h1>Javascript And React.js</h1>
+     <p>This was an amazing workshop taken up by Shaurya Sinha Sir.
+       We were taught everything from scratch including Javascript, React.js,html</p>
+       </div>
+  );
+}
+export default Note;
